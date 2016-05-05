3bc5ba13240b545f15db580af7f569d2d677ac3e
Florian Schätz <mail@florian-schaetz.de>
2016-05-05 11:07:02 +0200

    Added Spring Boot Application Class

---
+++ sdr-experiment/src/main/java/de/irian/learning/SdrExperimentApplication.java
@@ -0,0 +1,12 @@
+package de.irian.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdrExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdrExperimentApplication.class, args);
+	}
+}
